1e4f4701a5a9c2ee84d21155b6a1c6fd1719bef3
Stephanie <stephanie.grant.2008@gmail.com>
2021-09-16 18:26:18 +0000

    created new view for users, fixed syntax of user routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,10 @@ Rails.application.routes.draw do
 
     # These routes will be for signup. The first renders a form in the browse, the second will 
     # receive the form and create a user in our database using the data given to us by the user.
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  resources :users, only: [:new, :create] do
+    get :signup
+    post :users
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
