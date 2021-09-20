f54aca5fd0fad2b32c1261148df3a7b5333bd29a
Stephanie <stephanie.grant.2008@gmail.com>
2021-09-20 22:13:56 +0000

    test for validation of user name, email, and password all passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,36 @@ RSpec.describe User, type: :model do
       user.password_confirmation = "123ImTheBest"
       expect(user).to be_valid
     end
+
+    it "is not valid with a name" do
+      user = User.new
+
+      user.name = nil
+      user.email = "msalah@gmail.com"
+      user.password = "123ImTheBest"
+      user.password_confirmation = "123ImTheBest"
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      user = User.new
+
+      user.name = "Mo Salah"
+      user.email = nil
+      user.password = "123ImTheBest"
+      user.password_confirmation = "123ImTheBest"
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid without a password and matching password confirmation " do
+      user = User.new
+
+      user.name = "Mo Salah"
+      user.email = "msalah@gmail.com"
+      user.password = "123ImTheBess"
+      user.password_confirmation = "123ImTheBest"
+      expect(user).to_not be_valid
+    end
   
   end
 end
