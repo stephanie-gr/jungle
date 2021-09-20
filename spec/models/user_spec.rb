0db2d48a1ebd0d05c36d10d0e81912126c8d6fa6
Stephanie <stephanie.grant.2008@gmail.com>
2021-09-20 22:16:40 +0000

    test for unique email address passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,6 +34,26 @@ RSpec.describe User, type: :model do
       expect(user).to_not be_valid
     end
 
+    it "is not valid without a unique email" do
+      user = User.new
+      
+      user.name = "Mo Salah"
+      user.email = "msalah@gmail.com"
+      user.password = "123ImTheBest"
+      user.password_confirmation = "123ImTheBest"
+      expect(user).to be_valid
+
+      user.save
+      
+      user2 = User.new
+
+      user2.name = "Mo Salah"
+      user2.email = "msalah@gmail.com"
+      user2.password = "123ImTheBest"
+      user2.password_confirmation = "123ImTheBest"
+      expect(user2).to_not be_valid
+    end
+
     it "is not valid without a password and matching password confirmation " do
       user = User.new
 
